d5f09fd28c050b5d6a3a44f9a8ad816a6f1562f8
Gaymocoder <gaymocoder@gmail.com>
2025-03-12 19:26:43 +0800

    NB&NW | Added MainWindowWidget-inteface
    — Developed interface for MainWindowWidget;
    
    — Doesn't build.

--- include/Inklin/MainWindowWidget.h
+++ include/Inklin/MainWindowWidget.h
@@ -1,1 +1,34 @@
+#ifndef __INKLIN_MAIN_WINDOW_WIDGET_H__
+#define __INKLIN_MAIN_WINDOW_WIDGET_H__
 
+#include <QLabel>
+#include <QWidget>
+#include <QPushButton>
+
+namespace Inklin
+{
+    class FileTypeButton;
+    class FileSelectorWidget;
+    
+    class MainWindowWidget : public QWidget
+    {
+        Q_OBJECT;
+        
+        private:
+            QLabel* chosenFile;
+            QLabel* autoDefinedType;
+            
+            FileSelectorWidget* fileSelector;
+            
+            FileTypeButton* buttonAbsolute;
+            FileTypeButton* buttonDelta;
+            FileTypeButton* buttonAzimuth;
+            
+            QPushButton* buttonCalculate;
+            
+        public:
+            explicit MainWindowWidget(QWidget* parent = nullptr);
+    };
+}
+
+#endif
