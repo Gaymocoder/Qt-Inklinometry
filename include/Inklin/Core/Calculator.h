0f6bce7b824dc6900b058b1ceb01cdbb14e7c9b0
Gaymocoder <gaymocoder@gmail.com>
2025-03-12 22:05:28 +0800

    NB&NW | Calculator interface declaration
    — Declared Calculator interface (core part);
    
    — Doesn't build.

--- include/Inklin/Core/Calculator.h
+++ include/Inklin/Core/Calculator.h
@@ -1,1 +1,44 @@
+#ifndef __INKLIN_CORE_CALCULATOR_H__
+#define __INKLIN_CORE_CALCULATOR_H__
+
+#include <filesystem>
+
+#include <QWidget>
+
+namespace FS = std::filesystem;
+
+namespace Inklin
+{
+    enum DataFileType
+    {
+        ABSOLUTE = 0,
+        DELTA    = 1,
+        AZIMUTH  = 2,
+    };
+    
+    namespace Core
+    {
+        class Calculator : public QObject
+        {
+            Q_OBJECT;
+            
+            private:
+                FS::path file;
+                DataFileType fileType;
+                
+            public:
+                explicit Calculator(QWidget* parent = nullptr);
+            
+            signals:
+                void fireCalculationFinished();
+                
+            private slots:
+                void onFileChange(FS::path& newFile);
+                void onFileTypeChange(DataFileType newFileType);
+                void onCalculateRequest();
+        };
+    }
+}
+
+#endif
 
