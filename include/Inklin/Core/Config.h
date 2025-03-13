7493845b472e047283eebf6855d3131dcffd5784
Gaymocoder <gaymocoder@gmail.com>
2025-03-13 22:45:10 +0800

    NB&NW | THIS IS FUCKING BULLSHIT sorry
    — Realized uselessness of huge part of "Config"-implementation;
    — Realized having Woe from Wit;
    — Removed 60% of Config-code;
    
    — Doesn't build.

--- include/Inklin/Core/Config.h
+++ include/Inklin/Core/Config.h
@@ -11,42 +11,20 @@ namespace Inklin
 {
     namespace Core
     {
-        constexpr uint16_t configKeysAmount = 1;
-        
-        enum ConfigKeys
-        {
-            STARTPOS = 0, // Inklin::Core::DataSet
-        };
-        
         class Config
         {
             private:
                 const FS::path configFilePath;
-                void* config[configKeysAmount];
                 
             protected:
                 static std::string getStrKey(const std::string& line);
                 static std::string getStrValue(const std::string& line);
-                static uint16_t fromStrKey(const std::string& line);
-                
-                virtual void deleteValue(uint16_t key);
-                virtual void setDefaultValue(uint16_t key);
                 
             public:
                 Config(const FS::path& configPath);
-                
-                void setDefault();
-            
-                template <typename ValueType>
-                ValueType& getValue(uint16_t key, ValueType* ptr);
-                
-                template <typename ValueType>
-                void setValue(uint16_t key, ValueType value);
-                
+                                
                 virtual void save() const;
                 virtual void load();
-                
-                virtual ~Config();
         };
     }
 }
--- src/Inklin/Core/Config.cpp
+++ src/Inklin/Core/Config.cpp
@@ -4,8 +4,6 @@
 #include <fstream>
 #include <stdexcept>
 
-#define 
-
 using Inklin::SourceDataType;
 using namespace Inklin::Core;
 
@@ -13,83 +11,6 @@ Config::Config(const FS::path& configPath) : configFilePath(configPath)
 {
     this->setDefault();
     this->load();
-}
-
-Config::~Config()
-{
-    for(uint16_t key = 0; key < Inklin::Core::configKeysAmount; ++key)
-        this->deleteValue(key);
-}
-
-void Config::setDefault()
-{
-    for(uint16_t key = 0; key < Inklin::Core::configKeysAmount; ++key)
-        this->setDefaultValue(key);
-}
-
-void Config::deleteValue(uint16_t key)
-{
-    switch (key)
-    {
-        case STARTPOS:
-            delete (DataSet*) this->config[key];
-            break;
-            
-        default:
-            throw std::invalid_argument("Config::deleteValue(uint16_t): the config key does not exist");
-    }
-    
-    this->config[key] = nullptr;
-}
-
-void Config::setDefaultValue(uint16_t key)
-{
-    if (this->config[key])
-        this->deleteValue(key);
-        
-    switch (key)
-    {
-        case STARTPOS:
-            this->config[key] = new DataSet {0, 0, 0};
-            break;
-            
-        default:
-            throw std::invalid_argument("Config::setDefaultValue(uint16_t): the config key does not exist");
-    }
-    
-    this->config[key] = nullptr;
-}
-
-template <typename ValueType>
-ValueType& Config::getValue(uint16_t key, ValueType* ptr)
-{
-    ValueType** valuePtr = &(this->config[key]);
-    if (!(*valuePtr))
-        this->setDefaultValue(key);
-        
-    if (ptr)
-        *ptr = **valuePtr;
-        
-    return **valuePtr;
-}
-
-template <typename ValueType>
-void Config::setValue(uint16_t key, ValueType* value)
-{
-    if (!this->config[key])
-        this->config[key] = new ValueType(value);
-    else
-        *((ValueType*) (this->config[key])) = value;
-}
-
-void Config::setValue(uint16_t key, void* value)
-{
-    switch (key)
-    {
-        case STARTPOS:
-            *((DataSet*) this->config[key]) = *((DataSet*) value);
-            break;
-    }
 }
 
 std::string Config::getStrKey(const std::string& line)
@@ -102,18 +23,6 @@ std::string Config::getStrValue(const std::string& line)
     return line.substr(line.find("=") + 1);
 }
 
-uint16_t Config::fromStrKey(const std::string& key)
-{
-    switch (key)
-    {
-        case "STARTPOS":
-            return STARTPOS;
-            
-        default:
-            throw std::invalid_argument("Config::fromStrKey(const std::string&): the config key does not exist");
-    }
-}
-
 void Config::load() const
 {
     std::string lineBuf;
@@ -122,8 +31,5 @@ void Config::load() const
     {
         ConfigKeys key = Config::fromStrKey(Config::getStrKey(lineBuf));
         std::string strValue = Config::getStrValue(lineBuf);
-        
-        std::stringstream ssbuf(strValue);
-        ssbuf >> *(this->config[key]);
     }
 }
