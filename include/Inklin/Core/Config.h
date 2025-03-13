105e113cbee9530f83b5f4223a4ea9ea02e3c9ba
Gaymocoder <gaymocoder@gmail.com>
2025-03-13 15:04:43 +0800

    NB&NW | Remade Config-module architecture
    — Declared fully config-API interface;
    
    — Doesn't build.

--- include/Inklin/Core/Config.h
+++ include/Inklin/Core/Config.h
@@ -1,6 +1,8 @@
 #ifndef __INKLIN_CORE_CONFIG_H__
 #define __INKLIN_CORE_CONFIG_H__
 
+#include <map>
+#include <ctypes>
 #include <filesystem>
 
 namespace FS = std::filesystem;
@@ -9,18 +11,39 @@ namespace Inklin
 {
     namespace Core
     {
+        constexpr uint16_t configKeysAmount = 1;
+        
+        enum ConfigKeys
+        {
+            STARTPOS = 0, // Inklin::Core::DataSet
+        };
+        
         class Config
         {
             private:
                 const FS::path configFilePath;
                 
-            private:
-                DataSet startPosition;
+                void* config[configKeysAmount];
+                
+                static std::string getStrKey(std::string& line) const;
+                static std::string getStrValue(std::string& line) const;
+                
+            protected:
+                virtual void setDefault();
                 
             public:
-                Config(FS::path& configPath);
-                void save() const;
-                void load();
+                Config(const FS::path& configPath);
+                
+                template <typename ValueType>
+                void setValue(ConfigKeys key, ValueType value);
+            
+                template <typename ValueType>
+                ValueType getValue(ConfigKeys) const;
+                
+                virtual void save() const;
+                virtual void load();
+                
+                virtual ~Cofing();
         }
     }
 }
