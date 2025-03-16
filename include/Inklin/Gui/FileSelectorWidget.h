5694fe3cc5c1fe947b8788efc192f9c1b7d073f9
Gaymocoder <gaymocoder@gmail.com>
2025-03-16 20:38:04 +0800

    B&HW | Added interface for FileSelectorWidget | UT 21/23
    — Declared "FileSelectorWidget"-class;
    
    — ConfigTests:                10/10
        getStrKeyTest:              5/5
        getStrValueTest:            5/5
    — CalculatorTests:            11/13
        fromAbsolute_Test:          2/2
        fromDelta_Test:             2/2
        autoIdentifyFileType_Test:  3/3
        calculateAbsFile_Test:      2/2
        calculateDeltaFile_Test:    2/2
        calculateAzimFile_Test:     0/2
    
    — Builds, halfly works.

--- include/Inklin/Gui/FileSelectorWidget.h
+++ include/Inklin/Gui/FileSelectorWidget.h
@@ -1,1 +1,28 @@
+#ifndef __INKLIN_FILE_SELECTOR_WIDGET_H__
+#define __INKLIN_FILE_SELECTOR_WIDGET_H__
 
+#include "Inklin/SimpleTypes.h"
+
+#include <QPushButton>
+
+namespace Inklin
+{
+    namespace Gui
+    {
+        class FileSelectorWidget : public QFileDialog
+        {
+            Q_OBJECT;
+            
+            private:
+                SourceDataType dataType;
+                
+            public:
+                explicit FileSelectorWidget(QWidget* parent = nullptr);
+                
+            signals:
+                void fireFileSelected();
+        };
+    }
+}
+
+#endif
